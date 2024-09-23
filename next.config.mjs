c32fa90ca87272767cbef55a93aa65fac3c1e28a
WALWALASAFI <walwalasafi99@gmail.com>
2024-09-23 21:19:56 +0430

    work on errors

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: 'export',
+  // Add other configurations if needed
+};
 
 export default nextConfig;
